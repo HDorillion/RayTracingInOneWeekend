44171c071cfb02bbca125a3af3e51cadc8c16ada
Tyler Bird <tyler.b.bird@yahoo.com>
2020-05-09 17:37:08 -0500

    Conform to Snell's law for refraction and reflection

--- dialectric.cpp
+++ dialectric.cpp
@@ -12,7 +12,19 @@ bool Dialectric::scatter(
         (static_cast<int>
         (!record.faces_front) * ref_ind);
     vec3 unit_direction = unit_vector(r_in.direction());
-    vec3 refracted_ray = refract(unit_direction, record.normal, etai_over_etat);
-    scattered_ray = ray(record.p, refracted_ray);
-    return true;
+    // Follow Snell's Law to determine if reflection or refraction
+    double cos_theta = fmin(dot(-unit_direction, record.normal), 1.0);
+    double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+    // Calculate rays
+    if (etai_over_etat * sin_theta > 1.0) { // Reflect
+        vec3 reflected_ray = reflect(unit_direction, record.normal);
+        scattered_ray = ray(record.p, reflected_ray);
+        return true;
+    }
+    else { // Refract
+        vec3 refracted_ray =
+            refract(unit_direction, record.normal, etai_over_etat);
+        scattered_ray = ray(record.p, refracted_ray);
+        return true;
+    }
 }
--- world.h
+++ world.h
@@ -12,16 +12,16 @@ using std::make_shared;
 
 IntersectableList get_world() {
     IntersectableList world({
-    /*make_shared<Sphere>(
-        point3(0, 0, -1), 0.5, make_shared<Lambertian>(color(0.7,0.3,0.3))),*/
+    make_shared<Sphere>(
+        point3(0, 0, -1), 0.5, make_shared<Lambertian>(color(0.1,0.2,0.5))),
     make_shared<Sphere>(
         point3(0, -100.5, -1), 100, make_shared<Lambertian>(color(0.8,0.9,0.0))),
     make_shared<Sphere>(
-        point3(1, 0, -1), 0.5, make_shared<Metal>(color(0.8,0.6,0.2), 0.3)),
+        point3(1, 0, -1), 0.5, make_shared<Metal>(color(0.8,0.6,0.2), 0.0)),
     make_shared<Sphere>(
         point3(-1, 0, -1), 0.5, make_shared<Metal>(color(0.8,0.8,0.8), 1.0)),
     make_shared<Sphere>(
-        point3(0, 0, -1), 0.4, make_shared<Dialectric>(2.0)),
+        point3(-0.2, -0.1, -0.4), 0.2, make_shared<Dialectric>(1.5)),
     });
     return world;
 }
